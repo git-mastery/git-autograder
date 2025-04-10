4a712fc90328ab7b5427942c43cacefbcc2e4161
Jiahao, Woo <woojiahao1234@gmail.com>
2025-04-10 15:33:58 +0800

    Add has_deleted and has_added line helper methods

--- src/git_autograder/diff/diff.py
+++ src/git_autograder/diff/diff.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 
-from difflib_parser.difflib_parser import DiffParser
+from difflib_parser.difflib_parser import DiffCode, DiffParser
 from git.diff import Diff, Lit_change_type
 
 
@@ -14,3 +14,23 @@ class GitAutograderDiff:
     original_file: Optional[str]
     edited_file: Optional[str]
     diff_parser: Optional[DiffParser]
+
+    def has_deleted_line(self) -> bool:
+        if self.diff_parser is None:
+            return False
+
+        for diff in self.diff_parser.iter_diffs():
+            if diff.code == DiffCode.LEFT_ONLY and diff.line.strip() != "":
+                return True
+
+        return False
+
+    def has_added_line(self) -> bool:
+        if self.diff_parser is None:
+            return False
+
+        for diff in self.diff_parser.iter_diffs():
+            if diff.code == DiffCode.RIGHT_ONLY and diff.line.strip() != "":
+                return True
+
+        return False
